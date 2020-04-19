8baca7547ee3d0b2bd1ed628b1e517d2e892c7f9
Marcin Szuppe <marcin@iMac-Marcin.local>
2020-04-19 14:41:41 +0200

    Tak 3.7 Calculator test

---
+++ src/main/java/pl/mps/kodilla/basic_assertion/AssertionApplication.java
@@ -0,0 +1,33 @@
+package pl.mps.kodilla.basic_assertion;
+
+public class AssertionApplication {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a, b);
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int substractResult = calculator.subtract(a, b);
+        correct = ResultChecker.assertEquals(-3, substractResult);
+        if (correct) {
+            System.out.println("Metoda substract działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda substract nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+
+        int squareResult = calculator.square(a);
+        correct = ResultChecker.assertEquals(25, squareResult);
+        if (correct) {
+            System.out.println("Metoda square działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda square nie działa poprawnie dla liczb " + a);
+        }
+    }
+}
---
+++ src/main/java/pl/mps/kodilla/basic_assertion/ResultChecker.java
@@ -0,0 +1,8 @@
+package pl.mps.kodilla.basic_assertion;
+
+public class ResultChecker {
+
+    public static boolean assertEquals(int expected, int actual) {
+        return expected == actual;
+    }
+}
